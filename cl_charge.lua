36b9937e0698c57d950137dc94e35a62a1e93334
Randolio <103154494+Randolio@users.noreply.github.com>
2024-01-24 20:50:00 +0000

    reeeeee(qb-target support)

--- cl_charge.lua
+++ cl_charge.lua
@@ -1,5 +1,16 @@
 local chargeZones = {}
 
+local function removeAllZones()
+    for i = 1, #chargeZones do
+        if Config.OXTarget then
+            exports.ox_target:removeZone(chargeZones[i])
+        else
+            exports['qb-target']:RemoveZone(chargeZones[i])
+        end
+    end
+    table.wipe(chargeZones)
+end
+
 local function ChargeMenu(jobName)
     local jobLabel = QBCore.Shared.Jobs[jobName].label
     local response = lib.inputDialog(jobLabel, {
@@ -25,35 +36,60 @@ local function ChargeZones()
     for job, locations in pairs(Config.JobZones) do
         for i = 1, #locations do
             local v = locations[i]
-            local zone = exports.ox_target:addSphereZone({
-                coords = v.coords,
-                radius = 0.4,
-                debug = false,
-                options = {
-                    {
-                        icon = "fa-solid fa-money-check-dollar",
-                        groups = job,
-                        label = 'Charge Customer',
-                        onSelect = function()
-                            if not QBCore.Functions.GetPlayerData().jobb.onduty then
-                                return QBCore.Functions.Notify("You are not clocked in.", "error")
-                            end
-                            ChargeMenu(job)
-                        end,
-                        distance = 1.5,
+            if Config.OXTarget then
+                local zone = exports.ox_target:addSphereZone({
+                    coords = v.coords,
+                    radius = 0.4,
+                    debug = Config.Debug,
+                    options = {
+                        {
+                            icon = "fa-solid fa-money-check-dollar",
+                            groups = job,
+                            label = 'Charge Customer',
+                            onSelect = function()
+                                local PlayerJob = QBCore.Functions.GetPlayerData().job
+                                if not PlayerJob.onduty then
+                                    return QBCore.Functions.Notify("You are not clocked in.", "error")
+                                end
+                                ChargeMenu(job)
+                            end,
+                            distance = 1.5,
+                        },
+                    }
+                })
+                chargeZones[#chargeZones+1] = zone
+            else
+                local zone = exports['qb-target']:AddCircleZone("CHARGE_"..job..i, v.coords, 0.4,{ 
+                    name= "CHARGE_"..job..i, 
+                    debugPoly = Config.Debug, 
+                    useZ=true, 
+                }, {
+                    options = {
+                        {
+                            icon = "fa-solid fa-money-check-dollar",
+                            job = job,
+                            label = 'Charge Customer',
+                            action = function()
+                                local PlayerJob = QBCore.Functions.GetPlayerData().job
+                                if not PlayerJob.onduty then
+                                    return QBCore.Functions.Notify("You are not clocked in.", "error")
+                                end
+                                ChargeMenu(job)
+                            end,
+                        }
                     },
-                }
-            })
-            chargeZones[#chargeZones+1] = zone
+                    distance = 1.5
+                })
+                chargeZones[#chargeZones+1] = zone.name
+            end
         end
     end
 end
 
+
 AddEventHandler('onResourceStop', function(resourceName) 
 	if GetCurrentResourceName() == resourceName then
-        for k, v in pairs(chargeZones) do
-            exports.ox_target:removeZone(v)
-        end
+        removeAllZones()
 	end 
 end)
 
@@ -68,10 +104,7 @@ AddEventHandler('onResourceStart', function(resource)
 end)
 
 RegisterNetEvent('QBCore:Client:OnPlayerUnload', function()
-    for k, v in pairs(chargeZones) do
-        exports.ox_target:removeZone(v)
-    end
-    table.wipe(chargeZones)
+    removeAllZones()
 end)
 
 RegisterNetEvent("randol_billing:client:sendConfirm", function(data)
